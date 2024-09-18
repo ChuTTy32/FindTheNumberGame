e8bfe03b5b46a4d8305a100c768cbff568fe2866
Chutty32 <chutter.vi@gmail.com>
2024-09-18 23:38:06 +0300

    Добавлен проект с начальным(неправильным) и неполным функционалом

---
+++ FindTheNumberGame/FindTheNumberGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheNumberGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите начальное число: ");
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите конечное число: ");
+            int b;
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (b > a)
+            {
+                (a,b) = (b,a);
+            }
+        }
+    }
+}
