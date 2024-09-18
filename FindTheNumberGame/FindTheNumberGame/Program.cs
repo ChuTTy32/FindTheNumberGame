c4338bd70cb4c0df1662c6c757921b3f938dc9c6
Chutty32 <chutter.vi@gmail.com>
2024-09-19 00:50:58 +0300

    Добавление функции выбора сложности

--- FindTheNumberGame/FindTheNumberGame/Program.cs
+++ FindTheNumberGame/FindTheNumberGame/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Net;
 
 namespace FindTheNumberGame
 {
@@ -12,6 +14,40 @@ namespace FindTheNumberGame
             Console.WriteLine("Введите конечное число: ");
             int b = Convert.ToInt32(Console.ReadLine());
 
+            int attempts = 0;
+            bool validInput = false;
+
+            while (!validInput)
+            {
+                Console.WriteLine("Введите уровень сложности:" + "\n" +
+                    "1 - Легкий \t (20 попыток)" + "\n" +
+                    "2 - Средний \t (10 попыток)" + "\n" +
+                    "3 - Сложный \t (5 попыток)");
+
+                int level = Convert.ToInt32(Console.ReadLine());
+
+                switch (level)
+                {
+                    case 1:
+                        attempts = 20;
+                        validInput = true; // Завершаем цикл после правильного выбора
+                        break;
+                    case 2:
+                        attempts = 10;
+                        validInput = true; // Завершаем цикл после правильного выбора
+                        break;
+                    case 3:
+                        attempts = 5;
+                        validInput = true; // Завершаем цикл после правильного выбора
+                        break;
+                    default:
+                        Console.WriteLine("Неправильное значение. Повторите ввод:");
+                        break; // Цикл продолжается
+                }
+            }
+
+            Console.WriteLine($"Вы выбрали уровень сложности. У вас {attempts} попыток.");
+
             // Correct the swap logic if needed
             if (b < a)
             {
@@ -22,27 +58,43 @@ namespace FindTheNumberGame
             Random random = new Random();
             int answer = random.Next(a, b + 1);
 
+            int counter = 0;
+
             bool correctGuess = false;
 
-            while (!correctGuess)
+            while (attempts > 0 && !correctGuess)
             {
                 Console.WriteLine($"Угадайте число от {a} до {b}: ");
                 int guess = Convert.ToInt32(Console.ReadLine());
 
                 if (guess > answer)
                 {
-                    Console.WriteLine("Ваше число больше загаданного.");
+                    Console.WriteLine("Нужно меньше.");
                 }
                 else if (guess < answer)
                 {
-                    Console.WriteLine("Ваше число меньше загаданного.");
+                    Console.WriteLine("Нужно больше.");
                 }
                 else
                 {
-                    Console.WriteLine("Поздравляем! Вы угадали число.");
-                    correctGuess = true;
+                    Console.WriteLine("Ваш ответ верен! Поздравляем!");
+                    correctGuess = true;  // Число угадано
+                    break;  // Выходим из цикла
+                }
+
+                attempts--;  // Уменьшаем количество оставшихся попыток
+
+                // Если попытки исчерпаны и число не угадано
+                if (attempts == 0 && !correctGuess)
+                {
+                    Console.WriteLine("Число не было найдено. Попытки исчерпаны.");
                 }
             }
+
+            // Завершение программы
+            Console.WriteLine("Нажмите любую клавишу для завершения программы.");
+            Console.ReadKey();
+
         }
     }
 }
