377c31edf6724c9cd8be0aaf22e6c569f9fe8fd7
Chutty32 <chutter.vi@gmail.com>
2024-09-18 23:55:13 +0300

    Добавление базового функционала проекта

--- FindTheNumberGame/FindTheNumberGame/Program.cs
+++ FindTheNumberGame/FindTheNumberGame/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.ExceptionServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FindTheNumberGame
 {
@@ -11,18 +6,42 @@ namespace FindTheNumberGame
     {
         static void Main(string[] args)
         {
-            
             Console.WriteLine("Введите начальное число: ");
-            int a;
-            a = Convert.ToInt32(Console.ReadLine());
+            int a = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Введите конечное число: ");
-            int b;
-            b = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
 
-            if (b > a)
+            // Correct the swap logic if needed
+            if (b < a)
             {
-                (a,b) = (b,a);
+                (a, b) = (b, a);
+            }
+
+            // Generate a random number between a and b
+            Random random = new Random();
+            int answer = random.Next(a, b + 1);
+
+            bool correctGuess = false;
+
+            while (!correctGuess)
+            {
+                Console.WriteLine($"Угадайте число от {a} до {b}: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess > answer)
+                {
+                    Console.WriteLine("Ваше число больше загаданного.");
+                }
+                else if (guess < answer)
+                {
+                    Console.WriteLine("Ваше число меньше загаданного.");
+                }
+                else
+                {
+                    Console.WriteLine("Поздравляем! Вы угадали число.");
+                    correctGuess = true;
+                }
             }
         }
     }
